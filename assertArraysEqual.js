db55e130ea42fe4acbd63f1f60991d0514359c8d
maksym <maksym.polyakov@yahoo.ca>
2024-04-02 22:27:11 -0500

    assert array solution

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log("✅✅✅ Arrays are equal");
+  } else {
+    console.log("🛑🛑🛑 Arrays are not equal");
+  }
+};

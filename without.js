1af3652b78004b246bd8e098c8469017131547e7
maksym <maksym.polyakov@yahoo.ca>
2024-04-04 16:29:25 -0500

    without.js test cases add

--- without.js
+++ without.js
@@ -31,3 +31,6 @@ const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => should PASS
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // => should PASS
+

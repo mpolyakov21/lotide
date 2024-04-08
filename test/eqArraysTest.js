9965359f55bc699902562ea2981cd7c1f8e7d226
maksym <maksym.polyakov@yahoo.ca>
2024-04-08 18:33:23 -0500

    fix typo in second assert test

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,4 +2,4 @@ const assertEqual = require("../assertEqual");
 const eqArrays = require("../eqArrays");
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 1, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 1, 3], [1, 2, 3]), false);

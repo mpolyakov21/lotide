979db21529ae1a635d4366adc1bcf3432ba68d17
maksym <maksym.polyakov@yahoo.ca>
2024-04-08 18:00:39 -0500

    refactor to modular test'

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 2);
+
+
+module.exports = assertEqual;

353f26cf523650ac4665172cc1555a0db400117e
maksym <maksym.polyakov@yahoo.ca>
2024-04-08 18:31:23 -0500

    refactor eqArrays to modular

--- eqArrays.js
+++ eqArrays.js
@@ -9,13 +9,5 @@ const eqArrays = function(arr1, arr2) {
   }
   return true;
 };
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 1, 3], [1, 2, 3]), true);
+module.exports = eqArrays;

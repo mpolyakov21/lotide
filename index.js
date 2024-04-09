cddc337a67f390b1f7faf2cd39dda88ada6eaafb
maksym <maksym.polyakov@yahoo.ca>
2024-04-08 20:59:50 -0500

    add more functions to index.js

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
+const assertObjectsEqual: require('./assertObjectsEqual');
+const countLetters: require('./countLetters');
+const countOnly: require('./countOnly');
+const eqObjects: require('./eqObjects');
+const findKey: require('./findKey');
+const findKeyByValue: require('./findKeyByValue');
+const letterPositions: require('./letterPositions');
+const takeUntil: require('./takeUntil');
+const without: require('./without');
 
 module.exports = {
   head:   head,
@@ -12,4 +21,13 @@ module.exports = {
   assertArraysEqual: assertArraysEqual,
   assertEqual: assertEqual,
   eqArrays: eqArrays
+  assertObjectsEqual: assertObjectsEqual
+  countLetters: countLetters
+  countOnly: countOnly
+  eqObjects: eqObjects
+  findKey: findKey
+  findKeyByValue: findKeyByValue
+  letterPositions: letterPositions
+  takeUntil: takeUntil
+  without: without
 };
